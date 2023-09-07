20ae11b842a00d60933ef69efc030fa2a1fa29e5
shred-der <teminimighty@gmail.com>
2023-09-08 00:14:18 +0100

    api

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,21 @@ import type { NextAuthOptions } from 'next-auth'
 
 export const authOptions: NextAuthOptions = {
   // your configs
+  session: {
+    // Use JSON Web Tokens for session instead of database sessions.
+    // This option can be used with or without a database for users/accounts.
+    // Note: `strategy` should be set to 'jwt' if no database is used.
+    strategy: 'jwt'
+
+    // Seconds - How long until an idle session expires and is no longer valid.
+    // maxAge: 30 * 24 * 60 * 60, // 30 days
+
+    // Seconds - Throttle how frequently to write to database to extend a session.
+    // Use it to limit write operations. Set to 0 to always update the database.
+    // Note: This option is ignored if using JSON Web Tokens
+    // updateAge: 24 * 60 * 60, // 24 hours
+  },
+
 }
 
 export default NextAuth(authOptions);
